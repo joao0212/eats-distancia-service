1473821eae12e25b06e08885b7548923e6a6d5ee
João Victor <jv.martins@MacBook-Air-de-Joao.local>
2021-01-21 23:39:39 -0300

    ajuste nas classes grpc

--- src/main/java/br/com/caelum/eats/distancia/RecomendacaoGRpcService.java
+++ src/main/java/br/com/caelum/eats/distancia/RecomendacaoGRpcService.java
@@ -5,12 +5,11 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import br.com.caelum.eats.recomendacoes.grpc.Recomendacoes;
+import br.com.caelum.eats.recomendacoes.grpc.RecomendacoesDeRestaurantesGrpc;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-//import br.com.caelum.eats.recomendacoes.grpc.Recomendacoes.Restaurantes;
-//import br.com.caelum.eats.recomendacoes.grpc.RecomendacoesDeRestaurantesGrpc;
-//import br.com.caelum.eats.recomendacoes.grpc.RecomendacoesDeRestaurantesGrpc.RecomendacoesDeRestaurantesBlockingStub;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -20,7 +19,7 @@ public class RecomendacaoGRpcService {
 	private String recomendacaoServiceHost;
 	private Integer recomendacaoServicePort;
 	private ManagedChannel channel;
-	private RecomendacoesDeRestaurantesBlockingStub recomendacao;
+	private RecomendacoesDeRestaurantesGrpc.RecomendacoesDeRestaurantesBlockingStub recomendacao;
 
 	public RecomendacaoGRpcService(@Value("${recomendacoes.service.host}") String recomendacaoServiceHost,
 			@Value("${recomendacoes.service.port}") Integer recomendacaoServicePort) {
@@ -37,8 +36,8 @@ public class RecomendacaoGRpcService {
 	}
 
 	public List<Long> ordernarPorRecomendacoes(List<Long> idsRestaurante) {
-		Restaurantes restaurantes = Restaurantes.newBuilder().addAllRestauranteId(idsRestaurante).build();
-		Restaurantes restaurantesOrdenadosPorRecomendacao = recomendacao.recomendacoes(restaurantes);
+		Recomendacoes.Restaurantes restaurantes = Recomendacoes.Restaurantes.newBuilder().addAllRestauranteId(idsRestaurante).build();
+		Recomendacoes.Restaurantes restaurantesOrdenadosPorRecomendacao = recomendacao.recomendacoes(restaurantes);
 		List<Long> restaurantesOrdenados = restaurantesOrdenadosPorRecomendacao.getRestauranteIdList();
 
 		return restaurantesOrdenados;
